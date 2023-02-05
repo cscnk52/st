923c9e66c32cd6479d7a52bec815a0b437767195
cscnk52 <cscnk52@outlook.com>
2023-02-05 17:54:55 +0800

    feat(font): change st font to 14

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVuSansMono Nerd Font:pixelsize=12";
+static char *font = "DejaVuSansMono Nerd Font:pixelsize=14";
 static int borderpx = 2;
 
 /*
